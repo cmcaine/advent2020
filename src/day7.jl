e93f66f6d7b94968ed5b19e6b116c788e710dd31
Colin Caine <cmcaine@gmail.com>
2020-12-09 13:54:45 +0000

    day 7: minor changes

--- src/day7.jl
+++ src/day7.jl
@@ -1,5 +1,23 @@
 module day7
 
+"""
+This is the first time I've ever recorded myself programming, and this video is mostly for me to track and improve my programming, but maybe it'll interest others!I am sorry about the incomplete sentences, clacky keyboard and other bad bits, tho. Leave kind feedback below, if you like :)
+
+Before recording the video I read the problem description for part 1 and thought about it while I walked my dog. I hadn't read any other solutions or commentary.
+
+I was hoping to finish in 20-30 minutes but took about 90, so that's not great. I think I could have saved maybe 30 minutes by reading the problem descriptions more carefully and avoiding regex in the input processing step.
+
+The timeline is roughly this:
+
+0-
+13:00 - trying to load data, dead end with regexes
+25:00 - got data loaded in a simple way
+44:00 - got data loaded as a SimpleDiGraph
+58:00 - solved part 1
+1:15:00 - solved part 2 (wasted some time solving the wrong problem for each one)
+1:32:00 - cleaned some things up and faffed around
+"""
+
 # Input is an adjacency list for a weighted directed graph where nodes are bags of a given colour and edges show how many bags of what other colour they must contain.
 # Our task is to find all the bag colours that could contain our **shiny gold bag**.
 
@@ -20,11 +38,11 @@ function get_input(str=read("data/day7", String))
         return res
     end
     for line in split(str, '\n', keepempty=false)
-        thisbag, rest = split(line, " bags contain ")
+        thisbag, rest = split(line, " bags contain "; limit=2)
         src_node = ensure_bag_node(thisbag)
         rest == "no other bags." && continue
         edges = map(split(rest, r" bags?(, |.)", keepempty=false)) do edge
-            weight, destination = match(r"^(\d+) (.*)$", edge).captures
+            weight, destination = split(edge; limit=2)
             weight = parse(Int, weight)
             dst_node = ensure_bag_node(destination)
             add_edge!(g, src_node, dst_node, weight) || error("Couldn't add edge! ", join((thisbag, destination, dst_node, length(g), src_node), ", "))
